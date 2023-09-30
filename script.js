71c11975a20fadbda24d33c58ba21b4a531e5fe7
maulanaibnusaban <ibnusaban67@gmail.com>
2023-09-30 14:58:42 +0700

    menambahkan komentar

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
+// Fungsi untuk mendapatkan pilihan acak komputer: gajah, orang, atau semut.
 function getPilihanKomputer() {
   const comp = Math.random();
 
-  if (comp < 0.34) return "gajah";
-  if (comp >= 0.34 && comp < 0.7) return "orang";
-  return "semut";
+  if (comp < 0.34) return "gajah"; // Probabilitas untuk gajah: 0.34
+  if (comp >= 0.34 && comp < 0.7) return "orang"; // Probabilitas untuk orang: 0.36 (0.7 - 0.34)
+  return "semut"; // Probabilitas untuk semut: 0.30
 }
 
+// Fungsi untuk menentukan hasil permainan berdasarkan pilihan komputer dan pemain.
 function getHasil(comp, player) {
-  if (player == comp) return "SERI!";
-  if (player == "gajah") return comp == "orang" ? "MENANG!" : "KALAH!";
-  if (player == "orang") return (hasil = comp == "gajah" ? "KALAH!" : "MENANG!");
-  if (player == "semut") return comp == "orang" ? "KALAH!" : "MENANG!";
+  if (player == comp) return "SERI!"; // Pemain dan komputer memilih sama, seri.
+  if (player == "gajah") return comp == "orang" ? "MENANG!" : "KALAH!"; // Pemain pilih gajah, komputer pilih orang, pemain menang, dan sebaliknya.
+  if (player == "orang") return (hasil = comp == "gajah" ? "KALAH!" : "MENANG!"); // Pemain pilih orang, komputer pilih gajah, pemain kalah, dan sebaliknya.
+  if (player == "semut") return comp == "orang" ? "KALAH!" : "MENANG!"; // Pemain pilih semut, komputer pilih orang, pemain menang, dan sebaliknya.
 }
 
+// Fungsi untuk mengatur animasi putar gambar komputer.
 function putar() {
   const imgCom = document.querySelector(".img-komputer");
-  const gambar = ["gajah", "semut", "orang"];
+  const gambar = ["gajah", "semut", "orang"]; // Array gambar yang akan diputar.
   let i = 0;
-  const waktuMuai = new Date().getTime();
+  const waktuMulai = new Date().getTime();
+
+  // Mengatur interval untuk mengganti gambar dengan interval 100ms selama 1 detik (1000ms).
   setInterval(function () {
-    if (new Date().getTime() - waktuMuai > 1000) {
-      clearInterval;
+    if (new Date().getTime() - waktuMulai > 1000) {
+      clearInterval; // Menghentikan interval setelah 1 detik.
       return;
     }
+
+    // Mengganti gambar komputer sesuai dengan urutan dalam array gambar.
     imgCom.setAttribute("src", "img/" + gambar[i++] + ".png");
+
+    // Kembali ke gambar pertama jika sudah mencapai gambar terakhir.
     if (i == gambar.length) i = 0;
   }, 100);
 }
 
+// Mendapatkan semua elemen gambar untuk pilihan pemain dan menambahkan event listener untuk masing-masing.
 const pilihan = document.querySelectorAll("li img");
 pilihan.forEach(function (pil) {
   pil.addEventListener("click", function () {
+    // Mendapatkan pilihan acak komputer dan pilihan pemain.
     const pilihanComputer = getPilihanKomputer();
-    const pilihanplayer = pil.className;
-    const hasil = getHasil(pilihanComputer, pilihanplayer);
+    const pilihanPlayer = pil.className;
 
+    // Mendapatkan hasil permainan dan memulai animasi putar.
+    const hasil = getHasil(pilihanComputer, pilihanPlayer);
     putar();
 
+    // Menunggu 1 detik sebelum menampilkan hasil permainan dan gambar komputer.
     setTimeout(function () {
       const imgComputer = document.querySelector(".img-komputer");
       imgComputer.setAttribute("src", "img/" + pilihanComputer + ".png");
 
       const info = document.querySelector(".info");
-      info.innerHTML = hasil;
+      info.innerHTML = hasil; // Menampilkan hasil permainan.
     }, 1000);
   });
 });
